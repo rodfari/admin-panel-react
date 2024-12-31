c92b3b15c463682c784b07efe586e171f2307514
rodfari <rodrigo.code@outlook.com>
2024-12-31 00:20:21 -0300

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
 import { AppRouter } from './routes'
 
-function App() {
-
-  return <RouterProvider router={AppRouter} />
+const App = () => <RouterProvider router={AppRouter} />
   
-}
-
 export default App
